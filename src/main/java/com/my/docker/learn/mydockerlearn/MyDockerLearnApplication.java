aba366c720a4df6f305445e72a99358d977908fa
jeevanreddym <jeevan.rocks@gmail.com>
2022-07-21 07:36:05 -0400

    Docker learning.

---
+++ src/main/java/com/my/docker/learn/mydockerlearn/MyDockerLearnApplication.java
@@ -0,0 +1,13 @@
+package com.my.docker.learn.mydockerlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDockerLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDockerLearnApplication.class, args);
+    }
+
+}
